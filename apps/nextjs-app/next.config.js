60aac0e9ed036c10fe90b26ca985803f363bcb40
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2022-11-04 18:55:10 +0200

    chore: lint everything

--- apps/nextjs-app/next.config.js
+++ apps/nextjs-app/next.config.js
@@ -1,4 +1,4 @@
-//@ts-check
+// @ts-check
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const {withNx} = require("@nrwl/next/plugins/with-nx");
--- apps/nextjs-app/pages/_app.tsx
+++ apps/nextjs-app/pages/_app.tsx
@@ -2,17 +2,17 @@ import {AppProps} from "next/app";
 import Head from "next/head";
 import "./styles.css";
 
-function CustomApp({Component, pageProps}: AppProps) {
+const CustomApp = ({Component, pageProps}: AppProps) => {
     return (
         <>
             <Head>
                 <title>Welcome to nextjs-app!</title>
             </Head>
-            <main className="app">
+            <main className={"app"}>
                 <Component {...pageProps} />
             </main>
         </>
     );
-}
+};
 
 export default CustomApp;
