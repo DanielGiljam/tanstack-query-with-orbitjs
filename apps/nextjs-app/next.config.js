1fd5042ed968ae4ff28a49a0cc30150dab8b440c
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2023-02-18 20:17:21 +0200

    build: ignore TypeScript errors and ESLint errors when building
    https://nextjs.org/docs/api-reference/next.config.js/ignoring-typescript-errors
    https://nextjs.org/docs/api-reference/next.config.js/ignoring-eslint

--- apps/nextjs-app/next.config.js
+++ apps/nextjs-app/next.config.js
@@ -7,10 +7,16 @@ const {withNx} = require("@nrwl/next/plugins/with-nx");
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
     nx: {
         // Set this to true if you would like to to use SVGR
         // See: https://github.com/gregberge/svgr
         svgr: false,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
     },
 };
 
