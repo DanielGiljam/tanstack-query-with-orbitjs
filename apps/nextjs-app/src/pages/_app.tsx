763d0929b1fa370c5d9983ad86ed5c0a3eba81b4
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2023-02-18 18:51:41 +0200

    chore: configure MUI with Next.js
    https://github.com/mui/material-ui/tree/master/examples/material-next-ts

--- apps/nextjs-app/src/pages/_app.tsx
+++ apps/nextjs-app/src/pages/_app.tsx
@@ -1,16 +1,37 @@
+import {CacheProvider, EmotionCache} from "@emotion/react";
+import {CssBaseline, ThemeProvider} from "@mui/material";
 import {AppProps} from "next/app";
 import Head from "next/head";
+
+import {createEmotionCache, theme} from "../theme";
+
 import "./styles.css";
 
-const CustomApp = ({Component, pageProps}: AppProps) => {
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface CustomAppProps extends AppProps {
+    emotionCache?: EmotionCache;
+}
+
+const CustomApp = ({
+    Component,
+    pageProps,
+    emotionCache = clientSideEmotionCache,
+}: CustomAppProps) => {
     return (
         <>
             <Head>
                 <title>Welcome to nextjs-app!</title>
             </Head>
-            <main className={"app"}>
-                <Component {...pageProps} />
-            </main>
+            <CacheProvider value={emotionCache}>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <main className={"app"}>
+                        <Component {...pageProps} />
+                    </main>
+                </ThemeProvider>
+            </CacheProvider>
         </>
     );
 };
---
+++ apps/nextjs-app/src/theme/theme.ts
@@ -0,0 +1,28 @@
+import {createTheme} from "@mui/material";
+import {red} from "@mui/material/colors";
+import {Roboto} from "@next/font/google";
+
+export const roboto = Roboto({
+    weight: ["300", "400", "500", "700"],
+    subsets: ["latin"],
+    display: "swap",
+    fallback: ["Helvetica", "Arial", "sans-serif"],
+});
+
+// Create a theme instance.
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#556cd6",
+        },
+        secondary: {
+            main: "#19857b",
+        },
+        error: {
+            main: red.A400,
+        },
+    },
+    typography: {
+        fontFamily: roboto.style.fontFamily,
+    },
+});
