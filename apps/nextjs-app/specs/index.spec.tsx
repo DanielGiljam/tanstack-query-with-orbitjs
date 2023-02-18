abb2399f98e5f043c05c54009ca86e4a18b953ca
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2023-02-18 18:30:59 +0200

    refactor: move pages/ to src/pages/

--- apps/nextjs-app/specs/index.spec.tsx
+++ apps/nextjs-app/specs/index.spec.tsx
@@ -1,7 +1,7 @@
 import {render} from "@testing-library/react";
 import React from "react";
 
-import Index from "../pages/index";
+import Index from "../src/pages/index";
 
 describe("Index", () => {
     it("should render successfully", () => {
