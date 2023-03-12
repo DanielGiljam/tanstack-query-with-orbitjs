c6ea1f4aa61460c16076d23419102f7a97bd8ea3
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2023-03-12 08:39:36 +0200

    feat: add virtualization to note list

--- apps/demo-note-taking-app/src/components/NoteList/NoteList.tsx
+++ apps/demo-note-taking-app/src/components/NoteList/NoteList.tsx
@@ -1,8 +1,10 @@
 import {List} from "@mui/material";
+import {useVirtualizer} from "@tanstack/react-virtual";
 import React from "react";
 
 import {useNotes} from "../../hooks";
 
+import {LoadingListItem} from "./LoadingListItem";
 import {NoteListItem} from "./NoteListItem";
 
 export interface NoteListProps {
@@ -11,7 +13,12 @@ export interface NoteListProps {
 }
 
 export const NoteList = ({selectedNote, setSelectedNote}: NoteListProps) => {
-    const {data} = useNotes({
+    const {
+        data,
+        fetchNextPage,
+        hasNextPage = true,
+        isFetchingNextPage,
+    } = useNotes({
         onSuccess: (data) => {
             if (selectedNote == null) {
                 setSelectedNote(data.pages[0][0].id);
@@ -19,21 +26,77 @@ export const NoteList = ({selectedNote, setSelectedNote}: NoteListProps) => {
         },
         suspense: false,
     });
+    const notes = React.useMemo(() => data?.pages.flat() ?? [], [data]);
+    const parentRef = React.useRef<HTMLDivElement>(null);
+    const virtualizer = useVirtualizer({
+        count: hasNextPage ? notes.length + 1 : notes.length,
+        getScrollElement: () => parentRef.current,
+        estimateSize: () => 100,
+        overscan: 5,
+    });
+    const virtualItems = virtualizer.getVirtualItems();
+    React.useEffect(() => {
+        const [lastItem] = [...virtualItems].reverse();
+        if (
+            lastItem != null &&
+            lastItem.index >= notes.length - 1 &&
+            hasNextPage &&
+            !isFetchingNextPage
+        ) {
+            void fetchNextPage();
+        }
+    }, [
+        hasNextPage,
+        fetchNextPage,
+        notes.length,
+        isFetchingNextPage,
+        virtualItems,
+    ]);
     return (
-        <List
-            sx={{flexGrow: 1, overflowY: data != null ? "auto" : "hidden"}}
-            disablePadding
+        <div
+            ref={parentRef}
+            style={{contain: "strict", flexGrow: 1, overflowY: "auto"}}
         >
-            {(data?.pages[0] ?? Array.from(Array(8), () => undefined))
-                .flat()
-                .map((note, index) => (
-                    <NoteListItem
-                        key={note?.id ?? index}
-                        note={note}
-                        selectedNote={selectedNote}
-                        setSelectedNote={setSelectedNote}
-                    />
-                ))}
-        </List>
+            <div
+                style={{
+                    height: `${virtualizer.getTotalSize()}px`,
+                    position: "relative",
+                }}
+            >
+                <List
+                    style={{
+                        position: "absolute",
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        transform: `translateY(${virtualItems[0].start}px)`,
+                    }}
+                    disablePadding
+                >
+                    {virtualItems.map((virtualItem) => {
+                        if (virtualItem.index > notes.length - 1) {
+                            return (
+                                <LoadingListItem
+                                    key={"loading"}
+                                    ref={virtualizer.measureElement}
+                                    dataIndex={virtualItem.index}
+                                />
+                            );
+                        }
+                        const note = notes[virtualItem.index];
+                        return (
+                            <NoteListItem
+                                key={note?.id ?? virtualItem.index}
+                                ref={virtualizer.measureElement}
+                                dataIndex={virtualItem.index}
+                                note={note}
+                                selectedNote={selectedNote}
+                                setSelectedNote={setSelectedNote}
+                            />
+                        );
+                    })}
+                </List>
+            </div>
+        </div>
     );
 };
--- apps/demo-note-taking-app/src/components/NoteList/NoteListItem.tsx
+++ apps/demo-note-taking-app/src/components/NoteList/NoteListItem.tsx
@@ -10,78 +10,86 @@ import React from "react";
 import {Note} from "../../orbit";
 
 export interface NoteListItemProps {
+    dataIndex: number;
     note: Note | undefined;
     selectedNote: string | undefined;
     setSelectedNote: (id: string) => void;
 }
 
-export const NoteListItem = ({
-    note,
-    selectedNote,
-    setSelectedNote,
-}: NoteListItemProps) => {
-    const [isPending, startTransition] = React.useTransition();
-    const id = note?.id;
-    const title = note?.attributes.title;
-    const content = note?.attributes.content;
-    return (
-        <ListItem disablePadding>
-            <ListItemButton
-                selected={id != null && id === selectedNote}
-                dense
-                disableTouchRipple
-                divider
-                onClick={() => {
-                    if (id != null) {
-                        startTransition(() => setSelectedNote(id));
-                    }
-                }}
-            >
-                {isPending && (
-                    <LinearProgress
-                        sx={{position: "absolute", top: 0, left: 0, right: 0}}
+// eslint-disable-next-line react/display-name
+export const NoteListItem = React.forwardRef<HTMLLIElement, NoteListItemProps>(
+    (
+        {dataIndex, note, selectedNote, setSelectedNote}: NoteListItemProps,
+        ref,
+    ) => {
+        const [isPending, startTransition] = React.useTransition();
+        const id = note?.id;
+        const title = note?.attributes.title;
+        const content = note?.attributes.content;
+        return (
+            <ListItem ref={ref} data-index={dataIndex} disablePadding>
+                <ListItemButton
+                    selected={id != null && id === selectedNote}
+                    dense
+                    disableTouchRipple
+                    divider
+                    onClick={() => {
+                        if (id != null) {
+                            startTransition(() => setSelectedNote(id));
+                        }
+                    }}
+                >
+                    {isPending && (
+                        <LinearProgress
+                            sx={{
+                                position: "absolute",
+                                top: 0,
+                                left: 0,
+                                right: 0,
+                            }}
+                        />
+                    )}
+                    <ListItemText
+                        primary={
+                            title != null ? (
+                                title.length === 0 ? (
+                                    "Untitled"
+                                ) : (
+                                    title
+                                )
+                            ) : (
+                                <Skeleton />
+                            )
+                        }
+                        primaryTypographyProps={{
+                            noWrap: true,
+                            sx: [title?.length === 0 && {fontStyle: "italic"}],
+                        }}
+                        secondary={
+                            content != null ? (
+                                content.length === 0 ? (
+                                    "No content"
+                                ) : (
+                                    content
+                                )
+                            ) : (
+                                <Skeleton />
+                            )
+                        }
+                        secondaryTypographyProps={{
+                            sx: [
+                                {
+                                    display: "-webkit-box",
+                                    overflow: "hidden",
+                                    WebkitBoxOrient: "vertical",
+                                    WebkitLineClamp: "3",
+                                },
+                                content?.length === 0 && {fontStyle: "italic"},
+                            ],
+                        }}
                     />
-                )}
-                <ListItemText
-                    primary={
-                        title != null ? (
-                            title.length === 0 ? (
-                                "Untitled"
-                            ) : (
-                                title
-                            )
-                        ) : (
-                            <Skeleton />
-                        )
-                    }
-                    primaryTypographyProps={{
-                        noWrap: true,
-                        sx: [title?.length === 0 && {fontStyle: "italic"}],
-                    }}
-                    secondary={
-                        content != null ? (
-                            content.length === 0 ? (
-                                "No content"
-                            ) : (
-                                content
-                            )
-                        ) : (
-                            <Skeleton />
-                        )
-                    }
-                    secondaryTypographyProps={{
-                        sx: [
-                            {
-                                display: "-webkit-box",
-                                overflow: "hidden",
-                                WebkitBoxOrient: "vertical",
-                                WebkitLineClamp: "3",
-                            },
-                            content?.length === 0 && {fontStyle: "italic"},
-                        ],
-                    }}
-                />
-            </ListItemButton>
-        </ListItem>
-    );
-};
+                </ListItemButton>
+            </ListItem>
+        );
+    },
+);
