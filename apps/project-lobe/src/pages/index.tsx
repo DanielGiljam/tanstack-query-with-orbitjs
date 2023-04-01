7c7ec9cd17eb062714374bf4b350acb74100e7ed
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2023-04-01 17:55:53 +0300

    chore: fix and format code

--- apps/project-lobe/src/pages/index.tsx
+++ apps/project-lobe/src/pages/index.tsx
@@ -1,23 +1,23 @@
-import React from "react";
-import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import Layout from "@theme/Layout";
+import clsx from "clsx";
+import React from "react";
 
 import styles from "./index.module.css";
 
-function HomepageHeader() {
+const HomepageHeader = () => {
     const {siteConfig} = useDocusaurusContext();
     return (
         <header className={clsx("hero hero--primary", styles.heroBanner)}>
-            <div className="container">
-                <h1 className="hero__title">{siteConfig.title}</h1>
-                <p className="hero__subtitle">{siteConfig.tagline}</p>
+            <div className={"container"}>
+                <h1 className={"hero__title"}>{siteConfig.title}</h1>
+                <p className={"hero__subtitle"}>{siteConfig.tagline}</p>
                 <div className={styles.buttons}>
                     <Link
-                        className="button button--secondary button--lg"
-                        to="/docs/intro"
+                        className={"button button--secondary button--lg"}
+                        to={"/docs/intro"}
                     >
                         Docusaurus Tutorial - 5min ⏱️
                     </Link>
@@ -25,14 +25,14 @@ function HomepageHeader() {
             </div>
         </header>
     );
-}
+};
 
-export default function Home(): JSX.Element {
+const Home = () => {
     const {siteConfig} = useDocusaurusContext();
     return (
         <Layout
+            description={"Description will go into a meta tag in <head />"}
             title={`Hello from ${siteConfig.title}`}
-            description="Description will go into a meta tag in <head />"
         >
             <HomepageHeader />
             <main>
@@ -40,4 +40,6 @@ export default function Home(): JSX.Element {
             </main>
         </Layout>
     );
-}
+};
+
+export default Home;
