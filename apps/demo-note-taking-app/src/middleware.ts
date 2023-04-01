bacfecb8f6e304957b184b1a486bb6d2a9aa02c9
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2023-04-01 09:33:14 +0300

    fix: 404

--- apps/demo-note-taking-app/src/middleware.ts
+++ apps/demo-note-taking-app/src/middleware.ts
@@ -2,7 +2,9 @@ import {NextResponse} from "next/server";
 import type {NextRequest} from "next/server";
 
 export const middleware = async (request: NextRequest) =>
-    NextResponse.rewrite(new URL("/test-data.json", request.url));
+    NextResponse.rewrite(
+        new URL("/test-data.json", process.env.TEST_DATA_ORIGIN ?? request.url),
+    );
 
 export const config = {
     matcher: "/test-data.json",
