c4f83ed10684b638132f8c252a6fc93d8fc8a0c8
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2023-04-02 22:06:16 +0300

    revert: fix: 404
    This reverts commit bacfecb8f6e304957b184b1a486bb6d2a9aa02c9.

--- apps/demo-note-taking-app/src/middleware.ts
+++ apps/demo-note-taking-app/src/middleware.ts
@@ -2,9 +2,7 @@ import {NextResponse} from "next/server";
 import type {NextRequest} from "next/server";
 
 export const middleware = async (request: NextRequest) =>
-    NextResponse.rewrite(
-        new URL("/test-data.json", process.env.TEST_DATA_ORIGIN ?? request.url),
-    );
+    NextResponse.rewrite(new URL("/test-data.json", request.url));
 
 export const config = {
     matcher: "/test-data.json",
