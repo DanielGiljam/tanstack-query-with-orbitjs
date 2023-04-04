adfb4b8fbc9af0a9a6b97d4b359546ca9c0e0bb9
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2023-04-04 20:47:05 +0300

    fix: Prisma client multiple instances warning
    https://www.prisma.io/docs/guides/database/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices

--- apps/typical-chat-app-with-react-query/src/prisma/index.ts
+++ apps/typical-chat-app-with-react-query/src/prisma/index.ts
@@ -1,3 +1,13 @@
 import {PrismaClient} from "./client"; // eslint-disable-line import/no-relative-packages
 
-export const prisma = new PrismaClient();
+const globalForPrisma = global as unknown as {
+    prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+    globalForPrisma.prisma ??
+    new PrismaClient({
+        log: ["query"],
+    });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
