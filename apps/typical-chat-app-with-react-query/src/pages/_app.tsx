a55832813d3c7b3d370d0f63649523102a3a1c3d
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2023-04-05 10:34:11 +0300

    feat: title tag in head

--- apps/typical-chat-app-with-react-query/src/pages/_app.tsx
+++ apps/typical-chat-app-with-react-query/src/pages/_app.tsx
@@ -1,14 +1,20 @@
 import {QueryClientProvider} from "@tanstack/react-query";
 import type {AppProps} from "next/app";
+import Head from "next/head";
 
 import {queryClient} from "../query";
 
 import "../styles/global.css";
 
 const App = ({Component, pageProps}: AppProps) => (
-    <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-    </QueryClientProvider>
+    <>
+        <Head>
+            <title>Typical Chat App with React Query</title>
+        </Head>
+        <QueryClientProvider client={queryClient}>
+            <Component {...pageProps} />
+        </QueryClientProvider>
+    </>
 );
 
 export default App;
