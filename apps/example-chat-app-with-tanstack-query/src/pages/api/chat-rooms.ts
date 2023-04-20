bcd7e397c015d4dddcbade135742a3e6673e3346
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2023-04-20 09:17:09 +0300

    fix: add missing HTTP method check

--- apps/example-chat-app-with-tanstack-query/src/pages/api/chat-rooms.ts
+++ apps/example-chat-app-with-tanstack-query/src/pages/api/chat-rooms.ts
@@ -1,8 +1,12 @@
-import {NextApiResponse} from "next";
+import {NextApiRequest, NextApiResponse} from "next";
 
 import {prisma} from "../../prisma";
 
 const chatRooms = async (_req: never, res: NextApiResponse) => {
+    if (req.method !== "GET") {
+        res.status(405).json({statusMessage: "Method not allowed"});
+        return;
+    }
     const chatMessages = await prisma.chatMessage.findMany({
         distinct: ["chatRoomId"],
         orderBy: {createdAt: "desc"},
