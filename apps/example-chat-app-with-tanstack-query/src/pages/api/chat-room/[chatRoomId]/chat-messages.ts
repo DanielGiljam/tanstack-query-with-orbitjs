99ab5f31d185ec51cfce813fbd65499ad2e4355c
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2023-04-20 10:05:46 +0300

    feat: add pagination support to API endpoints

--- apps/example-chat-app-with-tanstack-query/src/pages/api/chat-room/[chatRoomId]/chat-messages.ts
+++ apps/example-chat-app-with-tanstack-query/src/pages/api/chat-room/[chatRoomId]/chat-messages.ts
@@ -1,7 +1,14 @@
 import {NextApiRequest, NextApiResponse} from "next";
+import {z} from "zod";
+import {fromZodError} from "zod-validation-error";
 
 import {prisma} from "../../../../prisma";
 import {idSchema} from "../../../../server";
+
+const nextQuerySchema = z.object({
+    count: z.coerce.number().int().finite().nonnegative().default(10),
+    offset: z.coerce.number().int().finite().nonnegative().default(0),
+});
 
 const chatMessages = async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method !== "GET") {
@@ -23,10 +30,19 @@ const chatMessages = async (req: NextApiRequest, res: NextApiResponse) => {
         res.status(404).json({statusMessage: "Not found"});
         return;
     }
+    const nextQueryParseResult = nextQuerySchema.safeParse(req.query);
+    if (!nextQueryParseResult.success) {
+        res.status(400).json({
+            statusMessage: "Bad request",
+            errorMessage: fromZodError(nextQueryParseResult.error).message,
+        });
+        return;
+    }
     const chatMessages = await prisma.chatMessage.findMany({
         where: {chatRoomId},
         orderBy: {createdAt: "desc"},
-        take: 10,
+        take: nextQueryParseResult.data.count,
+        skip: nextQueryParseResult.data.offset,
         include: {sender: true},
     });
     res.status(200).json(chatMessages);
--- apps/example-chat-app-with-tanstack-query/src/pages/api/chat-rooms.ts
+++ apps/example-chat-app-with-tanstack-query/src/pages/api/chat-rooms.ts
@@ -1,16 +1,32 @@
 import {NextApiRequest, NextApiResponse} from "next";
+import {z} from "zod";
+import {fromZodError} from "zod-validation-error";
 
 import {prisma} from "../../prisma";
 
-const chatRooms = async (_req: never, res: NextApiResponse) => {
+const nextQuerySchema = z.object({
+    count: z.coerce.number().int().finite().nonnegative().default(10),
+    offset: z.coerce.number().int().finite().nonnegative().default(0),
+});
+
+const chatRooms = async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method !== "GET") {
         res.status(405).json({statusMessage: "Method not allowed"});
+        return;
+    }
+    const nextQueryParseResult = nextQuerySchema.safeParse(req.query);
+    if (!nextQueryParseResult.success) {
+        res.status(400).json({
+            statusMessage: "Bad request",
+            errorMessage: fromZodError(nextQueryParseResult.error).message,
+        });
         return;
     }
     const chatMessages = await prisma.chatMessage.findMany({
         distinct: ["chatRoomId"],
         orderBy: {createdAt: "desc"},
-        take: 10,
+        take: nextQueryParseResult.data.count,
+        skip: nextQueryParseResult.data.offset,
         include: {chatRoom: true, sender: true},
     });
     const chatRooms = chatMessages.map(
