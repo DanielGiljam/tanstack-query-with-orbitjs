b1133d65765d6afed55b2f2aa1cfdec5f12aae20
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2023-04-27 13:25:19 +0300

    chore: remove build.lib.name's

--- libs/core/vite.config.ts
+++ libs/core/vite.config.ts
@@ -37,7 +37,6 @@ export default defineConfig({
         lib: {
             // Could also be a dictionary or array of multiple entry points.
             entry: "src/index.ts",
-            name: "tanstack-query-with-orbitjs-core",
             fileName: "index",
             // Change this to the formats you want to support.
             // Don't forgot to update your package.json as well.
