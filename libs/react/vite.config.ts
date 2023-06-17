3221a0e7a195c9d085d8aa19335bf3a52e35de8e
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2023-06-17 11:54:21 +0300

    build: remove build target and Vite build configuration

--- libs/react/vite.config.ts
+++ libs/react/vite.config.ts
@@ -1,22 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference types="vitest" />
 
-import {join} from "path";
-
 import {defineConfig} from "vite";
-import dts from "vite-plugin-dts";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
     cacheDir: "../../node_modules/.vite/react",
 
     plugins: [
-        dts({
-            tsConfigFilePath: join(__dirname, "tsconfig.lib.json"),
-            // Faster builds by skipping tests. Set this to false to enable type checking.
-            skipDiagnostics: true,
-        }),
-
         viteTsConfigPaths({
             root: "../../",
         }),
@@ -30,23 +21,6 @@ export default defineConfig({
     //    }),
     //  ],
     // },
-
-    // Configuration for building your library.
-    // See: https://vitejs.dev/guide/build.html#library-mode
-    build: {
-        lib: {
-            // Could also be a dictionary or array of multiple entry points.
-            entry: "src/index.ts",
-            fileName: "index",
-            // Change this to the formats you want to support.
-            // Don't forgot to update your package.json as well.
-            formats: ["es", "cjs"],
-        },
-        rollupOptions: {
-            // External packages that should not be bundled into your library.
-            external: [],
-        },
-    },
 
     test: {
         globals: true,
