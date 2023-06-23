291fd1a07c38d1399c6f47090fcbe21fa4057730
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2023-06-23 14:44:54 +0300

    build: configure tsup build target

---
+++ libs/core/tsup.config.ts
@@ -0,0 +1,55 @@
+import {exec} from "child_process";
+
+import * as devkit from "@nrwl/devkit";
+import {defineConfig} from "tsup";
+
+const {readJsonFile, writeJsonFile} = (
+    devkit as unknown as {default: typeof devkit}
+).default;
+
+const execAsync = async (command: string): Promise<void> =>
+    await new Promise((resolve, reject) => {
+        const childProcess = exec(command, (error) => {
+            if (error == null) {
+                resolve();
+            } else {
+                reject(error);
+            }
+        });
+        childProcess.stdout!.pipe(process.stdout);
+        childProcess.stderr!.pipe(process.stderr);
+    });
+
+const projectName = "core";
+const outDir = `../../dist/libs/${projectName}`;
+
+export default defineConfig({
+    clean: true,
+    dts: true,
+    entry: ["src/index.ts"],
+    format: "esm",
+    onSuccess: async () => {
+        console.log("onSuccess:");
+        await execAsync(`cp README.md ../../LICENSE ${outDir}`);
+        console.log("  - Copied README.md and ../../LICENSE to out directory.");
+        const pkg = await readJsonFile("package.json");
+        const rootPkg = await readJsonFile("../../package.json");
+        pkg.homepage = rootPkg.homepage.replace(
+            /#readme$/,
+            `/tree/main/libs/${projectName}#readme`,
+        );
+        pkg.bugs = rootPkg.bugs.concat(`+label%3A${projectName}`);
+        pkg.license = rootPkg.license;
+        pkg.author = rootPkg.author;
+        pkg.repository = {...rootPkg.repository};
+        pkg.repository.directory = `libs/${projectName}`;
+        await writeJsonFile(`${outDir}/package.json`, pkg);
+        console.log(
+            "  - Merged package.json and ../../package.json and wrote to out directory.",
+        );
+        console.log("  - Done.");
+    },
+    outDir,
+    outExtension: () => ({js: ".js"}),
+    skipNodeModulesBundle: true,
+});
---
+++ libs/react/tsup.config.ts
@@ -0,0 +1,82 @@
+import {exec} from "child_process";
+
+import * as devkit from "@nrwl/devkit";
+import {defineConfig} from "tsup";
+
+const {readJsonFile, writeJsonFile} = (
+    devkit as unknown as {default: typeof devkit}
+).default;
+
+const execAsync = async (command: string): Promise<void> =>
+    await new Promise((resolve, reject) => {
+        const childProcess = exec(command, (error) => {
+            if (error == null) {
+                resolve();
+            } else {
+                reject(error);
+            }
+        });
+        childProcess.stdout!.pipe(process.stdout);
+        childProcess.stderr!.pipe(process.stderr);
+    });
+
+const projectName = "react";
+const outDir = `../../dist/libs/${projectName}`;
+
+export default defineConfig(async () => {
+    const reactQueryPkg = await readJsonFile(
+        "../../node_modules/@tanstack/react-query/package.json",
+    );
+    const external: Array<string | RegExp> = [
+        "@tanstack-query-with-orbitjs/core",
+        ...new Set([
+            ...Object.keys(reactQueryPkg.dependencies ?? {}),
+            ...Object.keys(reactQueryPkg.peerDependencies ?? {}),
+        ]),
+    ].map((external) => new RegExp(`^${external}($|\\/|\\\\)`));
+    return {
+        clean: true,
+        dts: {
+            compilerOptions: {
+                rootDir: "../../",
+            },
+            respectExternal: true,
+        },
+        entry: ["src/index.ts"],
+        external,
+        format: "esm",
+        onSuccess: async () => {
+            console.log("onSuccess:");
+            await execAsync(`cp README.md ../../LICENSE ${outDir}`);
+            console.log(
+                "  - Copied README.md and ../../LICENSE to out directory.",
+            );
+            const pkg = await readJsonFile("package.json");
+            const rootPkg = await readJsonFile("../../package.json");
+            pkg.homepage = rootPkg.homepage.replace(
+                /#readme$/,
+                `/tree/main/libs/${projectName}#readme`,
+            );
+            pkg.bugs = rootPkg.bugs.concat(`+label%3A${projectName}`);
+            pkg.license = rootPkg.license;
+            pkg.author = rootPkg.author;
+            pkg.repository = {...rootPkg.repository};
+            pkg.repository.directory = `libs/${projectName}`;
+            pkg.dependencies = {
+                ...pkg.dependencies,
+                ...reactQueryPkg.dependencies,
+            };
+            pkg.peerDependencies = {
+                ...pkg.peerDependencies,
+                ...reactQueryPkg.peerDependencies,
+            };
+            await writeJsonFile(`${outDir}/package.json`, pkg);
+            console.log(
+                "  - Merged package.json and ../../package.json and wrote to out directory.",
+            );
+            console.log("  - Done.");
+        },
+        outDir,
+        outExtension: () => ({js: ".js"}),
+    };
+});
